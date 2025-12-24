8d72422888636e975ebe4ff91738ba79aa3c1de7
joobogyeong <   >
2025-12-25 01:29:33 +0900

    feat: add test file

---
+++ src/main/java/com/example/springproject1/test/test.java
@@ -0,0 +1,12 @@
+package com.example.springproject1.test;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class test {
+    @GetMapping("/test")
+    public String test(){
+        return "Hello Spring Boot!";
+    }
+}
