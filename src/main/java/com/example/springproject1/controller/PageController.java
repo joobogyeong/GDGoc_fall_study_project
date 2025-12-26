2fe9425e991d3bea403ada19879150f63c4d5a13
joobogyeong <162878426+joobogyeong@users.noreply.github.com>
2025-12-26 19:20:45 +0900

    mvp 연결 및 동작 확인

---
+++ src/main/java/com/example/springproject1/controller/PageController.java
@@ -0,0 +1,12 @@
+package com.example.springproject1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PageController {
+    @GetMapping("/")
+    public String returnHome() {
+        return "index";
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,15 @@
+<%@page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>black</title>
+</head>
+<body>
+<div>
+    <div>
+        <h2>공지사항</h2>
+    </div>
+</div>
+</body>
+</html>
